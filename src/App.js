7495d42a9439476111f4a19a39063f0d1a514161
Masud8851 <masud1971.cse@gmail.com>
2021-12-26 15:33:29 +0600

    Authentication

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./Components/Home/Home";
+import Login from "./Components/Login/Login";
 
 function App() {
-	return <div className="App"></div>;
+	return (
+		<div className="App">
+			<BrowserRouter>
+				<Routes>
+					<Route path="about" element={<Login />} />
+					<Route path="/" element={<Home />} />
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
